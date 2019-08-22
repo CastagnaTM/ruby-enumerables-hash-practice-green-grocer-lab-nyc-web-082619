5f424a8568759ba0f976c2b1a8c080f7f55a68f4
Thomas Castagna <castagna.tm@gmail.com>
2019-08-22 18:01:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,11 +42,11 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cart = consolidate_cart(cart)
-  cart = apply_coupons(cart, coupons)
-  cart = apply_clearance(cart)
+  con_cart = consolidate_cart(cart)
+  coup_cart = apply_coupons(con_cart, coupons)
+  clear_cart = apply_clearance(coup_cart)
   total = 0
-  cart.each do |n|
+  clear_cart.each do |n|
     total += n[1][:price]
   end
   total
